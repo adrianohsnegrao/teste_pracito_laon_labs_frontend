93675355770cca552cb2714974140c26145d2b53
Adriano Negrão <adrianohsn2022@gmail.com>
2025-01-23 14:39:03 -0400

    subindo com tela de login e autenticacao

---
+++ src/app/login/page.js
@@ -0,0 +1,78 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const getCsrfToken = async () => {
+    await axios.get('http://localhost:8000/api/sanctum/csrf-cookie');
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Obter o token CSRF antes de fazer a requisição de login
+      await getCsrfToken();
+
+      const response = await axios.post('http://localhost:8000/api/login', {
+        email,
+        password,
+      });
+      const { access_token } = response.data;
+
+      // Salve o token no localStorage ou em cookies
+      localStorage.setItem('authToken', access_token);
+
+      // Redirecione para a página principal
+      router.push('/');
+    } catch (err) {
+      setError(err.response?.data?.message || 'Something went wrong');
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-80">
+        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 p-2 w-full border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-sm font-medium text-gray-700">Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 p-2 w-full border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
